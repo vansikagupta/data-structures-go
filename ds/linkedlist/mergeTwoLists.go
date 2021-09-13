f8a06ce56f2065686e57fe70e03b8c4671b4a62e
vansgupt <vansgupt@in.ibm.com>
2021-09-13 20:25:14 +0530

    add: list problems

---
+++ ds/linkedlist/mergeTwoLists.go
@@ -0,0 +1,52 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// Inplace and iterative merge implementation
+// 1. first check both lists are non-empty; if not, return the non-nil list
+// 2. result list = min of both list's heads
+// 3. iterate through all elements in both lists till both are non-empty,
+// 4. append min node to result list and move forward
+// 5. repeat 4 and 5
+// 6. append the non-nil remaning list to result list
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	// both lists are non-empty
+	// head of resultant list; inplace merge
+	head := l2
+	if l1.Val < l2.Val {
+		head = l1
+		l1 = l1.Next
+	} else {
+		l2 = l2.Next
+	}
+	temp := head
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			temp.Next = l1
+			l1 = l1.Next
+		} else {
+			temp.Next = l2
+			l2 = l2.Next
+		}
+		temp = temp.Next
+	}
+	// either one or both lists are empty, append the remaining part
+	if l1 == nil {
+		temp.Next = l2
+	} else {
+		temp.Next = l1
+	}
+	return head
+}
---
+++ ds/linkedlist/removeNthFromEnd.go
@@ -0,0 +1,43 @@
+/*******
+leetcode question https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+0->1->2->3->4->5->nil
+Sol 1: find len in first traversal,
+       delete (len-n)th (0 index) element in second traversal
+
+Sol 2: Using two pointers, first pointer is at head and second ptr = (head + n)th node
+	   if ptr i nil, remove head node, else
+       ptr increments by 1
+	   keep increasing both pointers by 1 node till ptr = nil,
+       at this point first pointer is previous to the element to be deleted
+
+********/
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	ptr := head
+	for i := 0; i < n; i++ {
+		ptr = ptr.Next
+	}
+
+	// which means element to remove is the head node
+	if ptr == nil {
+		head = head.Next // there's no previous node
+		return head
+	}
+
+	prev := head
+	ptr = ptr.Next
+	for ptr != nil {
+		prev = prev.Next
+		ptr = ptr.Next
+	}
+	prev.Next = prev.Next.Next // x gets deleted
+	return head
+}
